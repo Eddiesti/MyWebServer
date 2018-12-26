506f037ffb03a4e3a13047583e754ec45e32134f
Eddiesti <37958895+Eddiesti@users.noreply.github.com>
2018-12-26 21:42:55 +0300

    Update DataInfoPageServlet.java

--- src/main/java/ru/otus/hibernate/servlets/DataInfoPageServlet.java
+++ src/main/java/ru/otus/hibernate/servlets/DataInfoPageServlet.java
@@ -1,58 +1,51 @@
 package ru.otus.hibernate.servlets;
 
-
-import ru.otus.hibernate.entity.AdressDataSet;
-import ru.otus.hibernate.entity.PhoneDataSet;
+import org.hibernate.ObjectNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.otus.hibernate.entity.UserDataSet;
 import ru.otus.hibernate.service.DBService;
 
-
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
-public class AddUserServlet extends HttpServlet {
+public class DataInfoPageServlet extends HttpServlet {
+    private static Logger logger = LoggerFactory.getLogger(AddUserServlet.class);
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        doPost(request, response);
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setCharacterEncoding("UTF-8");
+        Map<String, Object> pageVariables = new HashMap<>();
+
+        DBService service = (DBService) getServletContext().getAttribute("dbService");
+        TemplateProcessor templateProcessor = (TemplateProcessor) getServletContext().getAttribute("templateProcessor");
+
+        pageVariables.put("count", service.getCountUsers());
+
+        String idString = request.getParameter("id");
+        try {
+            if (idString != null && !idString.isEmpty()) {
+                String userById;
+
+                long id = Long.parseLong(idString);
+                userById = service.getUserById(id, UserDataSet.class);
+                if (userById != null) {
+                    pageVariables.put("name", userById);
+                }
+            }
+        } catch (ObjectNotFoundException ex) {
+            logger.error("User not fount");
+            response.sendRedirect("/error.html");
+        }
+        String pageText = templateProcessor.getPage("data_info.html", pageVariables);
+        response.getWriter().println(pageText);
     }
 
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        UserDataSet user = new UserDataSet();
-        DBService service = (DBService) getServletContext().getAttribute("dbService");
-        String name = request.getParameter("name");
-        if (name.equals("")) {
-            response.sendRedirect("/error.html");
-        }
+    public void doPost(HttpServletRequest request, HttpServletResponse response) {
 
-        String age = request.getParameter("age");
-        int parseAge = Integer.parseInt(age);
-        if (parseAge < 5 || parseAge > 100) {
-            response.sendRedirect("/error.html");
-        }
-        String number = request.getParameter("phone");
-
-        if (number.length() < 8) {
-            response.sendRedirect("/error.html");
-        }
-
-        String street = request.getParameter("adress");
-        user.setName(name);
-        user.setAge(parseAge);
-        AdressDataSet adress = new AdressDataSet();
-        adress.setStreet(street);
-        PhoneDataSet phone = new PhoneDataSet();
-        phone.setNumber(number);
-        phone.setUsers(user);
-        List<PhoneDataSet> list = new ArrayList<>();
-        list.add(phone);
-        user.setPhones(list);
-        user.setAdress(adress);
-        service.save(user);
-        response.sendRedirect("/data_info.html");
     }
 }
+
