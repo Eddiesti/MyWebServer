83944e5b12254e36a1ef7cc19f73b880c6094f64
Eddiesti <37958895+Eddiesti@users.noreply.github.com>
2018-12-26 21:42:25 +0300

    Update AddUserServlet.java

--- src/main/java/ru/otus/hibernate/servlets/AddUserServlet.java
+++ src/main/java/ru/otus/hibernate/servlets/AddUserServlet.java
@@ -14,8 +14,12 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AddUserServlet extends HttpServlet {
+    private static Logger logger = LoggerFactory.getLogger(AddUserServlet.class);
+
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         doPost(request, response);
@@ -26,17 +30,20 @@ public class AddUserServlet extends HttpServlet {
         DBService service = (DBService) getServletContext().getAttribute("dbService");
         String name = request.getParameter("name");
         if (name.equals("")) {
+            logger.error("Name is null");
             response.sendRedirect("/error.html");
         }
 
         String age = request.getParameter("age");
         int parseAge = Integer.parseInt(age);
         if (parseAge < 5 || parseAge > 100) {
+            logger.error("Age can't < 5 and > 100");
             response.sendRedirect("/error.html");
         }
         String number = request.getParameter("phone");
 
         if (number.length() < 8) {
+            logger.error("Phone number can't < 5");
             response.sendRedirect("/error.html");
         }
 
