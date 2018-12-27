e1ca95045375b91e4175862cbdd978e79919605a
Eddiesti <37958895+Eddiesti@users.noreply.github.com>
2018-12-27 22:21:57 +0300

    Update AddUserServlet.java

--- src/main/java/ru/otus/hibernate/servlets/AddUserServlet.java
+++ src/main/java/ru/otus/hibernate/servlets/AddUserServlet.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -30,21 +31,31 @@ public class AddUserServlet extends HttpServlet {
         DBService service = (DBService) getServletContext().getAttribute("dbService");
         String name = request.getParameter("name");
         if (name.equals("")) {
-            logger.error("Name is null");
+            logger.info("Name is null");
             response.sendRedirect("/error.html");
+            return;
         }
 
         String age = request.getParameter("age");
-        int parseAge = Integer.parseInt(age);
-        if (parseAge < 5 || parseAge > 100) {
-            logger.error("Age can't < 5 and > 100");
+        int parseAge = 0;
+        try {
+             parseAge = Integer.parseInt(age);
+        } catch (NumberFormatException ex) {
+            logger.info("Incorrect correct data");
             response.sendRedirect("/error.html");
+            return;
+        }
+        if (parseAge < 5 || parseAge > 100) {
+            logger.info("Age can't < 5 and > 100");
+            response.sendRedirect("/error.html");
+            return;
         }
         String number = request.getParameter("phone");
 
         if (number.length() < 8) {
-            logger.error("Phone number can't < 5");
+            logger.info("Phone number can't < 5");
             response.sendRedirect("/error.html");
+            return;
         }
 
         String street = request.getParameter("adress");
@@ -61,5 +72,7 @@ public class AddUserServlet extends HttpServlet {
         user.setAdress(adress);
         service.save(user);
         response.sendRedirect("/data_info.html");
+        return;
     }
 }
+
